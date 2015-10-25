50a646210921bc428ed9f833f9292ebd1206f564
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-10-25 17:37:09 +0100

    It works !

--- connect-behavior.js
+++ connect-behavior.js
@@ -1,7 +1,27 @@
-/** @polymerBehavior */
+/* jshint ignore:start */
 'use strict';
 
-Polymer.ReduxConnectBehavior = function (mapStateToProps) {
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var defaultMapStateToProps = function defaultMapStateToProps() {
+	return {};
+};
+var defaultMapDispatchToProps = function defaultMapDispatchToProps(dispatch) {
+	return { dispatch: dispatch };
+};
+var defaultMergeProps = function defaultMergeProps(stateProps, dispatchProps, parentProps) {
+	return _extends({}, parentProps, stateProps, dispatchProps);
+};
+
+/** @polymerBehavior */
+Polymer.ReduxConnectBehavior = function (mapStateToProps, mapDispatchToProps, mergeProps) {
+	var options = arguments.length <= 3 || arguments[3] === undefined ? {} : arguments[3];
+
+	var shouldSubscribe = Boolean(mapStateToProps);
+	var finalMapStateToProps = mapStateToProps || defaultMapStateToProps;
+	var finalMapDispatchToProps = mapDispatchToProps || defaultMapDispatchToProps;
+	var finalMergeProps = mergeProps || defaultMergeProps;
+
 	return {
 		properties: {
 			store: {
@@ -18,16 +38,37 @@ Polymer.ReduxConnectBehavior = function (mapStateToProps) {
 
 			this.async(function () {
 				var provider = _this.create('redux-provider');
-				var store = _this.store = provider.store;
+				var store = _this._store = provider.store;
 
 				store.subscribe(_this._handleNewState.bind(_this));
 
-				_this._computeStateProps(store);
+				_this._updateStateProps();
+				_this._updateDispatchProps();
+				Object.assign(_this, _this._computeNextState(_this._stateProps, _this._dispatchProps));
 			}, 1);
 		},
 
 		_handleNewState: function _handleNewState() {
-			console.log(this.store.getState());
+			Object.assign(this, this._store.getState());
+		},
+
+		_computeNextState: function _computeNextState(stateProps, dispatchProps) {
+			var mergedProps = finalMergeProps(stateProps, dispatchProps);
+			return mergedProps;
+		},
+
+		_updateStateProps: function _updateStateProps() {
+			var props = arguments.length <= 0 || arguments[0] === undefined ? this : arguments[0];
+
+			var nextStateProps = this._computeStateProps(this._store, props);
+			this._stateProps = nextStateProps;
+		},
+
+		_updateDispatchProps: function _updateDispatchProps() {
+			var props = arguments.length <= 0 || arguments[0] === undefined ? this : arguments[0];
+
+			var nextDispatchProps = this._computeDispatchProps(this._store, props);
+			this._dispatchProps = nextDispatchProps;
 		},
 
 		_computeStateProps: function _computeStateProps(store, props) {
@@ -35,6 +76,14 @@ Polymer.ReduxConnectBehavior = function (mapStateToProps) {
 			var stateProps = mapStateToProps(state, props);
 
 			return stateProps;
+		},
+
+		_computeDispatchProps: function _computeDispatchProps(store, props) {
+			var dispatch = store.dispatch;
+
+			var dispatchProps = finalMapDispatchToProps(dispatch, props);
+
+			return dispatchProps;
 		}
 	};
 };
